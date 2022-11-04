c9f27ba62f260e0ce93fbc4dd6305c5e910fdd7e
Elena Lukashova <ele.lukashova@gmail.com>
2022-11-04 16:38:33 +0100

    feat: add error handling

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,19 +4,24 @@ const path = require('path');
 const folderPath = path.join(__dirname, 'secret-folder');
 
 (async () => {
-  const files = await fs.promises.readdir(folderPath,{withFileTypes: true});
-  const notFolders = files.filter(file => !file.isDirectory());
+  try {
+    const files = await fs.promises.readdir(folderPath,{withFileTypes: true});
+    const notFolders = files.filter(file => !file.isDirectory());
 
-  for (let file of notFolders) {
-    const fileName = file.name.toString();
-    const filePath = path.join(__dirname, './secret-folder', fileName);
-    const name = path.parse(fileName).name;
-    const ext = path.extname(fileName).replace(".","");
+    for (let file of notFolders) {
+      const fileName = file.name.toString();
+      const filePath = path.join(__dirname, './secret-folder', fileName);
+      const name = path.parse(fileName).name;
+      const ext = path.extname(fileName).replace(".","");
 
-    const stats = await fs.promises.stat(filePath);
-    const size = Number(stats.size / 1000);
+      const stats = await fs.promises.stat(filePath);
+      const size = Number(stats.size / 1000);
 
-    const result = `${name} - ${ext} - ${size}kb`;
-    console.log(result);
+      const result = `${name} - ${ext} - ${size}kb`;
+      console.log(result);
+    } 
+
+  } catch (err) {
+      console.log(err);
   }
 })();
